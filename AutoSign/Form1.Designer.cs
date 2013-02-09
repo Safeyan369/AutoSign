e554c16bd898b6e83f15c16eb023109db95b8585
Fed Kwan <fedkwan@gmail.com>
2013-02-10 03:15:55 +0800

    百度贴吧自动签到 C# 开源代码

---
+++ AutoSign/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AutoSign
+{
+    partial class AutoSign
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoSign));
+            this.start = new System.Windows.Forms.Button();
+            this.tips = new System.Windows.Forms.Label();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.start.Location = new System.Drawing.Point(42, 69);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 25);
+            this.start.TabIndex = 1;
+            this.start.Text = "开始签到";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // tips
+            // 
+            this.tips.AutoSize = true;
+            this.tips.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tips.ForeColor = System.Drawing.Color.Black;
+            this.tips.Location = new System.Drawing.Point(35, 24);
+            this.tips.Name = "tips";
+            this.tips.Size = new System.Drawing.Size(92, 17);
+            this.tips.TabIndex = 102;
+            this.tips.Text = "请点击开始签到";
+            // 
+            // wb
+            // 
+            this.wb.Location = new System.Drawing.Point(1, 1);
+            this.wb.MinimumSize = new System.Drawing.Size(1, 1);
+            this.wb.Name = "wb";
+            this.wb.ScrollBarsEnabled = false;
+            this.wb.Size = new System.Drawing.Size(1, 1);
+            this.wb.TabIndex = 100;
+            // 
+            // AutoSign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(164, 111);
+            this.Controls.Add(this.tips);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.wb);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutoSign";
+            this.Text = "贴吧自动签到";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label tips;
+        private System.Windows.Forms.WebBrowser wb;
+    }
+}
+
---
+++ AutoSign/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using zoyobar.shared.panzer;
+using zoyobar.shared.panzer.web;
+using zoyobar.shared.panzer.web.ib;
+
+namespace AutoSign
+{
+    public partial class AutoSign : Form
+    {
+        public AutoSign()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            tips.Text = "准备签到...";
+
+            string baidu_wapp = "http://wapp.baidu.com";
+
+            IEBrowser ie = new IEBrowser(this.wb);
+            ie.Navigate(baidu_wapp);
+            ie.IEFlow.Wait(new UrlCondition("wait", baidu_wapp, StringCompareMode.StartWith), 10);
+
+            ie.InstallJQuery(JQuery.CodeMin);
+            ie.ExecuteJQuery(JQuery.Create("'a'"), "__jAs");
+            int more_count = ie.ExecuteJQuery<int>(JQuery.Create("__jAs").Length());
+
+            string href = "";
+            for (int index = 0; index < more_count; index++)
+            {
+                ie.ExecuteJQuery(JQuery.Create("__jAs").Eq(index.ToString()), "__jA");
+                if (ie.ExecuteJQuery<string>(JQuery.Create("__jA").Text()) == "更多>>")
+                {
+                    href = ie.ExecuteJQuery<string>(JQuery.Create("__jA").Attr("'href'"));
+                }
+            }
+
+            if (href.Length > 0)
+            {
+                string[] more_url_array = href.Split('/');
+                string pre_bar = baidu_wapp + "/" + more_url_array[1] + @"/" + more_url_array[2] + @"/";
+
+                string my_favorite = baidu_wapp + href;
+                ie.Navigate(my_favorite);
+                ie.IEFlow.Wait(new UrlCondition("wait", my_favorite, StringCompareMode.StartWith));
+
+                ie.InstallJQuery(JQuery.CodeMin);
+                ie.ExecuteJQuery(JQuery.Create("'a'"), "__jBs");
+                int fav_count = ie.ExecuteJQuery<int>(JQuery.Create("__jBs").Length());
+
+                List<string> fav_bar = new List<string>();
+                for (int index = 0; index < fav_count; index++)
+                {
+                    ie.ExecuteJQuery(JQuery.Create("__jBs").Eq(index.ToString()), "__jB");
+                    string fav_text = ie.ExecuteJQuery<string>(JQuery.Create("__jB").Text());
+                    string fav_url = pre_bar + ie.ExecuteJQuery<string>(JQuery.Create("__jB").Attr("'href'"));
+                    if (fav_text != "发言记录" && fav_text != "贴吧" && fav_text != "百度")
+                    {
+                        fav_bar.Add(fav_url);
+                    }
+                }
+                int num = 0;
+                foreach (string bar_url in fav_bar)
+                {
+                    num++;
+                    tips.Text = "进度：" + num + "/" + fav_bar.Count;
+
+                    ie.Navigate(bar_url);
+                    ie.IEFlow.Wait(new UrlCondition("wait", bar_url, StringCompareMode.StartWith));
+
+                    ie.InstallJQuery(JQuery.CodeMin);
+                    ie.ExecuteJQuery(JQuery.Create("'a'"), "__jCs");
+                    int sign_count = ie.ExecuteJQuery<int>(JQuery.Create("__jCs").Length());
+                    for (int index = 0; index < sign_count; index++)
+                    {
+                        ie.ExecuteJQuery(JQuery.Create("__jCs").Eq(index.ToString()), "__jC");
+                        string sign_text = ie.ExecuteJQuery<string>(JQuery.Create("__jC").Text());
+                        string sign_url = baidu_wapp + ie.ExecuteJQuery<string>(JQuery.Create("__jC").Attr("'href'"));
+                        if (sign_text == "签到")
+                        {
+                            ie.Navigate(sign_url);
+                            ie.IEFlow.Wait(new UrlCondition("wait", sign_url, StringCompareMode.StartWith));
+                        }
+                    }
+                }
+                tips.Text = fav_bar.Count + "签到完毕！";
+            }
+        }
+    }
+}
